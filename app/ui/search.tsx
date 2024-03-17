56ca899a492dcb06cfad92fe390ee1aae174e3b8
srisathvik <komminenisathvik@gmail.com>
2024-03-17 18:30:38 +0530

    mutating data

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,13 +9,13 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`);
+    // console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
     params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
-      param.delete('query');
+      params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
   }, 300);
