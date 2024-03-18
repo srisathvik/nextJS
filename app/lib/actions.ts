dd204a4946ea5065926c2b3476b05f591b20a916
srisathvik <komminenisathvik@gmail.com>
2024-03-18 18:55:49 +0530

    adding authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,9 @@ import { z } from 'zod';
 import {sql} from "@vercel/postgres"
 import {revalidatePath} from "next/cache"
 import {redirect} from "next/navigation"
- 
+import {signIn} from "@/auth"
+import {AuthError} from "next-auth"
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
@@ -29,6 +31,26 @@ export type State = {
   };
   message?: string | null;
 };
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    console.log('actions.ts authenticate error', error);
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 
 export async function createInvoice(prevState: State, formData: FormData) {
   const validatedFields = CreateInvoice.safeParse({
@@ -75,7 +97,7 @@ export async function updateInvoice(id: string,
       message: 'Missing Fields. Failed to Create Invoice.',
     };
   }
-  // const { customerId, amount, status } = validatedFields.data;
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
  
   try {
